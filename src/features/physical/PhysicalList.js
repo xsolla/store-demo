c38a61c40df4821a5af3fd6d59272c89af5ad431
Nikita Bonachev <n.bonachev@xsolla.com>
2020-02-04 16:37:27 +0500

    feat [COMMERCE-3127] add multiple multiple pg to cart

--- src/features/physical/PhysicalList.js
+++ src/features/physical/PhysicalList.js
@@ -1,107 +1,136 @@
-import React, { PureComponent } from "react";
+import React, {PureComponent} from "react";
 
 import {PhysicalItem} from './PhysicalItem';
 import {getPsTokenByItem} from '../StoreLoader';
 import {getPhysicalGoods} from './PhysicalListLoader';
 
 import './PhysicalList.css';
+import Product from "../../components/Product";
+import {ProductConsumer} from "../../context";
 
 export class PhysicalList extends PureComponent {
-  constructor() {
-    super();
-    
-    this.state = {
-      isPurchasing: false
+    constructor() {
+        super();
+
+        this.state = {
+            isPurchasing: false
+        }
     }
-  }
-  
-  componentDidMount() {
-    if (this.props.logToken && null === this.props.physicalItems) {
-      this.updateCatalogue();
-      this.props.updateVirtualCurrencyBalance();
+
+    componentDidMount() {
+        if (this.props.logToken && null === this.props.physicalItems) {
+            this.updateCatalogue();
+            this.props.updateVirtualCurrencyBalance();
+        }
     }
-  }
-  
-  componentWillUnmount() {
-    if (null !== this.props.physicalItems) {
-      this.props.setPhysicalItems(null);
+
+    componentWillUnmount() {
+        if (null !== this.props.physicalItems) {
+            this.props.setPhysicalItems(null);
+        }
     }
-  }
-  
-  updateCatalogue() {
-    getPhysicalGoods(window.xProjectId, this.props.logToken)
-        .then(physicalItems => {
-          this.props.setPhysicalItems(physicalItems);
-        })
-        .catch(() => {
-          this.props.setPhysicalItems([]);
-        });
-  }
-  
-  buyItem(item) {
-    if (this.state.isPurchasing) {
-      return;
-    }
-    
-    const {logToken} = this.props;
-    this.setState({
-      isPurchasing: true
-    })
-    getPsTokenByItem(item, logToken)
-      .then(response => {
-        let options = {
-          'height' : 680
-        };
-        window.xPayStationInit(response.data["token"], options);
-        window.XPayStationWidget.open();
-        window.XPayStationWidget.on(
-          window.XPayStationWidget.eventTypes.CLOSE,
-          () => {
-            this.setState({
-              isPurchasing: false
+
+    updateCatalogue() {
+        getPhysicalGoods(window.xProjectId, this.props.logToken)
+            .then(physicalItems => {
+                this.props.setPhysicalItems(physicalItems);
+            })
+            .catch(() => {
+                this.props.setPhysicalItems([]);
             });
-          }
-        );
-      })
-      .catch(e => {
+    }
+
+    buyItem(item) {
+        if (this.state.isPurchasing) {
+            return;
+        }
+
+        const {logToken} = this.props;
         this.setState({
-          isPurchasing: false
+            isPurchasing: true
         })
-      });
-  };
-  
-  render() {
-    const {physicalItems} = this.props;
-    
-    return (
-      <div>
-        <div className="">
-          <div className="physical-list">
-            {
-              physicalItems && physicalItems.length &&
-                physicalItems
-                  .map(
-                    (oneProduct, key) => {
-                      return (
-                        <PhysicalItem
-                          key={oneProduct.sku}
-                          order={key}
-                          initClass="initialFlow1"
-                          title={oneProduct.name}
-                          description={oneProduct.description}
-                          imageUrl={oneProduct.image_url}
-                          quantity={oneProduct.quantity}
-                          currency={oneProduct.price.currency}
-                          price={oneProduct.price.amount}
-                          handleBuyByRealCurrency={() => this.buyItem(oneProduct)}
-                        />
-                      );
+        getPsTokenByItem(item, logToken)
+            .then(response => {
+                let options = {
+                    'height': 680
+                };
+                window.xPayStationInit(response.data["token"], options);
+                window.XPayStationWidget.open();
+                window.XPayStationWidget.on(
+                    window.XPayStationWidget.eventTypes.CLOSE,
+                    () => {
+                        this.setState({
+                            isPurchasing: false
+                        });
                     }
-                  )
-            }
-          </div>
-        </div>
-      </div>
-    );
-  }
+                );
+            })
+            .catch(e => {
+                this.setState({
+                    isPurchasing: false
+                })
+            });
+    };
+
+    render() {
+        const {physicalItems} = this.props;
+
+        return (
+            <div>
+                <ProductConsumer>
+                    {
+                        valueFromContext =>
+                            <div className="">
+                                <div className="physical-list">
+                                    {
+                                        physicalItems && physicalItems.length &&
+                                        physicalItems
+                                            .map(
+                                                (oneProduct, key) => {
+                                                    return (
+                                                        <Product
+                                                            ref={this.ProductRef}
+                                                            key={oneProduct.sku}
+                                                            order={key}
+                                                            initClass="initialFlow1"
+                                                            sku={oneProduct.sku}
+                                                            title={oneProduct.name}
+                                                            description={
+                                                                oneProduct.description
+                                                            }
+                                                            price={oneProduct.price.amount}
+                                                            image_url={oneProduct.image_url}
+                                                            currency={
+                                                                oneProduct.price.currency
+                                                            }
+                                                            product={oneProduct}
+                                                            addToCart={
+                                                                valueFromContext.addToCart
+                                                            }
+                                                            getTheme={valueFromContext.getTheme.bind(
+                                                                this
+                                                            )}
+                                                            cartId={valueFromContext.cartId}
+                                                            logToken={
+                                                                valueFromContext.logToken
+                                                            }
+                                                            changeItemQuantityInCart={
+                                                                valueFromContext.changeItemQuantityInCart
+                                                            }
+                                                            activeGroup={this.props.activeGroup}
+                                                            buyByVC={
+                                                                valueFromContext.buyByVC
+                                                            }
+                                                        />
+                                                    );
+                                                }
+                                            )
+                                    }
+                                </div>
+                            </div>
+                    }
+                </ProductConsumer>
+            </div>
+        );
+    }
 }
