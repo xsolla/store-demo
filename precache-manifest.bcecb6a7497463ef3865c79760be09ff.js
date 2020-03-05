c099c47d40cc2b9787398ffe2dbcc6c2e8bd5155
Stepan <rainbowprodder@gmail.com>
2020-03-05 13:46:49 +0700

    Updates

---
+++ precache-manifest.bcecb6a7497463ef3865c79760be09ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5b7dc1f2657a9c951f041964d4f37d",
+    "url": "/store-demo/index.html"
+  },
+  {
+    "revision": "3f762ec37f543ef8d20a",
+    "url": "/store-demo/static/css/main.1d2a70cb.chunk.css"
+  },
+  {
+    "revision": "9b79a7656bca5e7dfc92",
+    "url": "/store-demo/static/js/2.f964c91a.chunk.js"
+  },
+  {
+    "revision": "b72268eb14f5e809860c94e6f21928c0",
+    "url": "/store-demo/static/js/2.f964c91a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f762ec37f543ef8d20a",
+    "url": "/store-demo/static/js/main.0c5868a2.chunk.js"
+  },
+  {
+    "revision": "7a8a7c63ae3b84ca7468",
+    "url": "/store-demo/static/js/runtime-main.cbe36886.js"
+  }
+]);
